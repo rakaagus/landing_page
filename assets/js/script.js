ef0f3d9708d2b07dde66a3adb0d9f990cb950244
rakhaagus <kakakunyukya@gmail.com>
2021-12-05 23:30:24 +0700

    Mengupload file tugas landing page (1 / 5)

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let searchForm = document.querySelector('.search-form-container');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+let cart = document.querySelector('.shopping-cart-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cart.classList.toggle('active');
+}
